256280028743676ebcfd7fb0926247038aef377f
tflaten4184 <tflaten4184@gmail.com>
2023-03-23 15:02:37 -0700

    bootstrap and styling, cards

--- imageshare/imageshare/settings.py
+++ imageshare/imageshare/settings.py
@@ -135,5 +135,8 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
